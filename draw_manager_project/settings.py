667841bcb5ba9a9c7d26af0f74e2013fc9b08af3
hiro92 <hirofumi.4.4.19@gmail.com>
2020-06-27 21:14:00 +0900

    20200627

--- draw_manager_project/settings.py
+++ draw_manager_project/settings.py
@@ -10,19 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 from .settings_common import *
-import os
+# import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from django.contrib.messages import constants as messages
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+# from django.contrib.messages import constants as messages
+#
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#
+#
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gos-mt2*$n3fmfy-g0!+o%r7)w^1(9c72naz1&-jtt20^74!#x'
+# SECRET_KEY = 'gos-mt2*$n3fmfy-g0!+o%r7)w^1(9c72naz1&-jtt20^74!#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -32,107 +32,107 @@ ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'draw_manager_app.apps.DrawManagerAppConfig',
-    'accounts.apps.AccountsConfig',
-
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-
-    'django_ses',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'draw_manager_project.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'draw_manager_project.wsgi.application'
-
-
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#
+#     'draw_manager_app.apps.DrawManagerAppConfig',
+#     'accounts.apps.AccountsConfig',
+#
+#     'django.contrib.sites',
+#     'allauth',
+#     'allauth.account',
+#
+#     'django_ses',
+# ]
+#
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+#
+# ROOT_URLCONF = 'draw_manager_project.urls'
+#
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+#
+# WSGI_APPLICATION = 'draw_manager_project.wsgi.application'
+#
+#
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'draw_manager',
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': '',
-        'PORT': '',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'draw_manager',
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+#
+#
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+# LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'Asia/Tokyo'
+# TIME_ZONE = 'Asia/Tokyo'
 
-USE_I18N = True
+# USE_I18N = True
 
-USE_L10N = True
+# USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 STATIC_ROOT = '/usr/share/nginx/html/static'
 MEDIA_ROOT = '/usr/share/nginx/html/media'
@@ -185,48 +185,48 @@ LOGGING = {
     }
 }
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
-
-MESSAGE_TAGS = {
-    messages.ERROR: 'alert alert-danger',
-    messages.WARNING: 'alert alert-warning',
-    messages.SUCCESS: 'alert alert-success',
-    messages.INFO: 'alert alert-info',
-}
-
-AUTH_USER_MODEL = 'accounts.CustomUser'
-
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+# )
+#
+# MESSAGE_TAGS = {
+#     messages.ERROR: 'alert alert-danger',
+#     messages.WARNING: 'alert alert-warning',
+#     messages.SUCCESS: 'alert alert-success',
+#     messages.INFO: 'alert alert-info',
+# }
+#
+# AUTH_USER_MODEL = 'accounts.CustomUser'
+#
 # allauthの設定
 #django-allauthで利用するdjango.contrib.sitesを使うためにサイト識別用IDを設定
-SITE_ID = 1
+# SITE_ID = 1
 
-AUTHENTICATION_BACKENDS = (
-    'allauth.account.auth_backends.AuthenticationBackend',
+# AUTHENTICATION_BACKENDS = (
+#     'allauth.account.auth_backends.AuthenticationBackend',
 #一般ユーザー用(メールアドレス)
-    'django.contrib.auth.backends.ModelBackend',
+    # 'django.contrib.auth.backends.ModelBackend',
 #管理サイト用(ユーザー名認証)
-)
+# )
 
 #メールアドレス認証に変更するように設定
-ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_USERNAME_REQUIRED = False
+# ACCOUNT_AUTHENTICATION_METHOD = 'email'
+# ACCOUNT_USERNAME_REQUIRED = False
 
 #サインアップにメールアドレス確認を挟むように設定
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_EMAIL_REQUIRED = True
+# ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+# ACCOUNT_EMAIL_REQUIRED = True
 
 #ログイン/ログアウト後の遷移先を設定
-LOGIN_REDIRECT_URL = 'draw:top_page'
-ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
+# LOGIN_REDIRECT_URL = 'draw:top_page'
+# ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
 
 #ログアウトリンクのクリック一発でログアウトする設定
-ACCOUNT_LOGOUT_ON_GET = True
+# ACCOUNT_LOGOUT_ON_GET = True
 
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
 #バックアップバッチ用
-BACKUP_PATH = 'backup/'
-NUM_SAVED_BACKUP = 30
+# BACKUP_PATH = 'backup/'
+# NUM_SAVED_BACKUP = 30
